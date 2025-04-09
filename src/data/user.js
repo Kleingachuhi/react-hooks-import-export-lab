6e2335a477d4b488d3f099f1b592fc24d57cd284
Klein <klein.gachuhi@student.moringaschool.com>
2025-04-09 12:26:49 +0300

    Reactions

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,7 @@
-const username = "Liza";
+import React from "react";
+const username =  "Liza"
 const city = "New York";
+const image =  "https://i.imgur.com/mV8PQxj.gif";
+    // export all three variables as named exports
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
-
-// export all three variables as named exports
+export {username, city, image}
